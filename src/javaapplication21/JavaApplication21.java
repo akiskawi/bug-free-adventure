e3395edf30370deef6b7b8b86aef3d1135df0403
Ioannis P Poikilidis <akiskawi@gmail.com>
2022-05-05 19:33:23 +0200

    Daphnnee Commit

--- src/javaapplication21/JavaApplication21.java
+++ src/javaapplication21/JavaApplication21.java
@@ -16,6 +16,7 @@ public class JavaApplication21 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World Moth**F**ker!");
+        System.out.println("Daphne kati ekana edw des to");
     }
     
 }
